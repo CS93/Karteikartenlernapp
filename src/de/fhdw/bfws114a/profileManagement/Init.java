bea34f986a4f4221790756a12e08ec0d82d455cf
CS93 <Carsten.Schlender@web.de>
2015-11-18 16:37:00 +0100

    Übergang Login zu Profile Management implementieren

--- src/de/fhdw/bfws114a/profileManagement/Init.java
+++ src/de/fhdw/bfws114a/profileManagement/Init.java
@@ -1,9 +1,24 @@
 package de.fhdw.bfws114a.profileManagement;
 
 import android.app.Activity;
+import android.os.Bundle;
+import de.fhdw.bfws114asc.counter1.R;
 
 public class Init extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//nur zum Testen
+		setContentView(R.layout.activity_profile_management);
+
+		
+	}
 	
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+//		mData.saveDataInBundle(outState);
+		super.onSaveInstanceState(outState);
+	}
 
 }
