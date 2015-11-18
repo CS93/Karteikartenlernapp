7d483ea29834c74da7bea3fb48691c47e80a0c1d
CS93 <Carsten.Schlender@web.de>
2015-11-18 16:45:00 +0100

    Fertigstellung vom userMenu

--- src/de/fhdw/bfws114a/classManagement/Init.java
+++ src/de/fhdw/bfws114a/classManagement/Init.java
@@ -2,12 +2,23 @@ package de.fhdw.bfws114a.classManagement;
 
 import android.app.Activity;
 import android.os.Bundle;
+import de.fhdw.bfws114asc.counter1.R;
 
 public class Init extends Activity {
 
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);			
+		super.onCreate(savedInstanceState);
+		//nur zum Testen
+		setContentView(R.layout.activity_settings_class_management);
+
+		
 	}
+	
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+//		mData.saveDataInBundle(outState);
+		super.onSaveInstanceState(outState);
+	}
+
 }
