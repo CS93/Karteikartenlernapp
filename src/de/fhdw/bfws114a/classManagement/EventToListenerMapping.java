6a16b3354aa901ee4bf81d904e79599fb5a8f39b
A114AD3 <A114AD3@KOENB10523.koeln.ads.risnet.de>
2015-12-01 11:26:33 +0100

    profileManagement aufrufen, Daten von DB anzeigen

---
+++ src/de/fhdw/bfws114a/classManagement/EventToListenerMapping.java
@@ -0,0 +1,32 @@
+package de.fhdw.bfws114a.classManagement;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import de.fhdw.bfws114a.classManagement.ApplicationLogic;
+import de.fhdw.bfws114a.classManagement.Gui;
+import de.fhdw.bfws114a.lernKartei.R;
+
+public class EventToListenerMapping  implements OnClickListener {
+
+	private ApplicationLogic mApplicationLogic;
+
+	public EventToListenerMapping(Gui gui, ApplicationLogic applicationLogic){
+		mApplicationLogic = applicationLogic;
+		gui.getDefaultClassButton().setOnClickListener(this);
+		gui.getSaveClassButton().setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch ( v.getId()){
+		case R.id.b_default_class_management:
+			mApplicationLogic.onDefaultClassClicked();
+			break;
+		case R.id.b_save_class_management:
+			mApplicationLogic.onSaveClassClicked();
+			break;
+		}
+		
+	}
+	
+}
